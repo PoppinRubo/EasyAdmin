29c49326734d38144bb43c9cf706e77200cc6169
PoppinRubo <hiphopbl@aliyun.com>
2018-07-17 13:02:45 +0800

    add Admin.php

---
+++ items/application/admin/controller/Admin.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Admin extends Controller
+{
+    public function __construct()
+    {
+        //管理系统基类，在此处方法预处理
+
+    }
+}
