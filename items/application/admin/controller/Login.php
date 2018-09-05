010dd2c77d700f9a0f18ae9a10e7a9de718bc75d
PoppinRubo <hiphopbl@aliyun.com>
2018-09-05 18:57:47 +0800

    update login

--- items/application/admin/controller/Login.php
+++ items/application/admin/controller/Login.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\admin\controller;
 
+use think\captcha\Captcha;
 use think\Controller;
 
 class Login extends Admin
@@ -8,5 +9,23 @@ class Login extends Admin
     public function index()
     {
         return View();
+    }
+
+    public function captcha()
+    {
+        $captcha = new Captcha();
+        //验证码位数
+        $captcha->length = 4;
+        //验证成功后是否重置
+        $captcha->reset = true;
+        //验证码字体大小(px)
+        $captcha->fontSize = 17;
+        //验证码过期时间（s）
+        $captcha->expire = 120;
+        //验证码图片宽度，设置为0为自动计算
+        $captcha->imageW = 120;
+        //验证码图片高度，设置为0为自动计算
+        $captcha->imageH = 40;
+        return $captcha->entry();
     }
 }
