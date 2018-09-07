3ad5efb9616a0a41070da0083692f1db206a6c3b
PoppinRubo <hiphopbl@aliyun.com>
2018-09-07 18:20:10 +0800

    update login

--- items/application/admin/controller/Login.php
+++ items/application/admin/controller/Login.php
@@ -6,18 +6,26 @@ use think\Controller;
 
 class Login extends Admin
 {
+    //登录 view
     public function index()
     {
         return View();
     }
 
+    //登录
     public function signIn()
     {
         if (request()->isGet()) {
-            return toJsonData(0, null, "不被接受的请求",true);
+            return toJsonData(0, null, "不被接受的请求", true);
         }
+        if (!$this->checkCaptcha(input('vercode'))) {
+            return toJsonData(0, null, "验证码错误");
+        }
+        $user = db('sys_user')->where(array("Account" => input("account"), "Password" => input("password")))->find();
+        return toJsonData(0, $user, "验证码错误");
     }
 
+    //生成验证码
     public function captcha()
     {
         $captcha = new Captcha();
@@ -33,6 +41,33 @@ class Login extends Admin
         $captcha->imageW = 120;
         //验证码图片高度，设置为0为自动计算
         $captcha->imageH = 40;
-        return $captcha->entry();
+        return $captcha->entry('signIn');
+    }
+
+    //检测输入的验证码是否正确
+    private function checkCaptcha($code)
+    {
+        $captcha = new Captcha();
+        return $captcha->check($code, 'signIn');
+    }
+
+    //退出登录
+    public function signOut()
+    {
+        if (getUserAuthentication() != null) {
+            // 清除session（当前作用域）
+            Session::clear();
+            if (getUserAuthentication() != null) {
+                return toJsonData(0, null, "退出时出现问题，请稍后重试！");
+            }
+            //清理cookie记录,马上过期
+            if (setcookie("AutoVerify", "Clear", time(), "/")) {
+                return toJsonData(1);
+            } else {
+                return toJsonData(-2, null, "HipHop:登录状态清除失败,可手动清理浏览器cookie");
+            }
+        } else {
+            return toJsonData(1);
+        }
     }
 }
