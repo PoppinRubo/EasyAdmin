6bcf0c3a026d4439531f2cbd62acc3dc3dedf2b7
PoppinRubo <hiphopbl@aliyun.com>
2018-09-10 14:07:00 +0800

    update login/index.html

--- items/application/admin/controller/Login.php
+++ items/application/admin/controller/Login.php
@@ -35,7 +35,7 @@ class Login extends Admin
             if (getUserAuthentication() == null) {
                 return toJsonData(0, null, "登录失败,请联系管理员");
             }
-            return toJsonData(1, null, "登录成功");
+            return toJsonData(1, "/admin/home", "登录成功");
         } catch (Exception $e) {
             return toJsonData(0, null, $e->getMessage());
         }
