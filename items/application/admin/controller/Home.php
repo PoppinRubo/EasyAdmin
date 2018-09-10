707cba7157197ebb817bb1033749135acd5251d3
PoppinRubo <hiphopbl@aliyun.com>
2018-09-10 14:17:02 +0800

    add home

---
+++ items/application/admin/controller/Home.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Home extends Admin
+{
+    //系统主页 view
+    public function index()
+    {
+        return View();
+    }
+}
