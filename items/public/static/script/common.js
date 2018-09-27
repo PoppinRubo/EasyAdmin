74298a50c3ccfb16d4d65bf22499038a1bd1def3
PoppinRubo <hiphopbl@aliyun.com>
2018-09-27 13:42:16 +0800

    优化细节、增加助手方法

--- items/public/static/script/common.js
+++ items/public/static/script/common.js
@@ -5,8 +5,23 @@ function ajaxErrMsg(xhr, textStatus) {
     layer.msg("请求出现错误,错误状态码：" + textStatus);
 }
 /*
-表格高度适配
+表格助手
 */
-function tableHeight(table = "table") {
-    return parseInt($(window).height() - $("#" + table).offset().top - 35);
-}
+var tableHelper = {
+    //高度适配
+    setHeight: function(table = "data-table") {
+        return parseInt($(window).height() - $("#" + table).offset().top - 35);
+    },
+    choose: function(tr) {
+        var selection = tr.find("input[type='checkbox'],input[type='radio']");
+        $.each(selection, function(i, v) {
+            //点击一下渲染好的选框
+            $(v).next().on("click", function(e) {
+                //阻止事件冒泡
+                e.stopPropagation();
+            }).click();
+        });
+        //标注选中样式
+        tr.addClass('layui-table-click').siblings().removeClass('layui-table-click');
+    }
+};
