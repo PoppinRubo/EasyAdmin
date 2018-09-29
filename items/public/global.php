47dbfca737c9eb22a1ba2407a7c8f2aa2627e8a8
PoppinRubo <hiphopbl@aliyun.com>
2018-09-29 19:40:22 +0800

    easyui 表格融入

--- items/public/global.php
+++ items/public/global.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
     <link rel="stylesheet" href="/static/layui/css/layui.css" media="all">
-    <link rel="stylesheet" href="/static/easyui/themes/bootstrap/easyui.css" media="all">
+    <link rel="stylesheet" href="/static/easyui/style/easyui.css" media="all">
+    <link rel="stylesheet" href="/static/style/easySkin.css" media="all">
     <link rel="stylesheet" href="/static/style/menuTree.css" media="all">
     <link rel="stylesheet" href="/static/icomoon/style.css" media="all">
-    <link rel="stylesheet" href="/static/style/admin.css" media="all">   
+    <link rel="stylesheet" href="/static/style/admin.css" media="all">
     <script type="text/javascript" src="/static/script/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="/static/script/jquery.cookie.js"></script>
     <script type="text/javascript" src="/static/easyui/jquery.easyui.min.js"></script>
+    <script type="text/javascript" src="/static/easyui/easyui-lang-zh_CN.js"></script>
     <script type="text/javascript" src="/static/layui/layui.js"></script>
     <script type="text/javascript" src="/static/script/common.js"></script>
