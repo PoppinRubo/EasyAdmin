a455b51e3693bb491f5e4abb496711c7287fcbe1
PoppinRubo <hiphopbl@aliyun.com>
2018-11-20 18:58:58 +0800

    items/public/static/script/datagrid.extend.js

--- items/public/static/script/datagrid.extend.js
+++ items/public/static/script/datagrid.extend.js
@@ -7,26 +7,32 @@ $.extend($.fn.datagrid.defaults.view, {
         var options = $(target).datagrid('options');
         var singleSelect = options.singleSelect;
         var view = $(target).closest(".datagrid-view");
-        var checkbox = view.find("input[type=checkbox]");
+        var checkbox = view.find("tr input[type=checkbox]");
         if (singleSelect) {
             //表头选框去除
             view.find(".datagrid-header-check").html("");
             //单选模式
             $.each(checkbox, function(i, v) {
-                $(v)[0].type = "radio";
+                var parent = $(v).parent();
+                parent.html('<input type="radio" name="radio">');
+                var radio = parent.find("input[type=radio]");
                 //添加样式
-                $(v).addClass("layui-hide").parent().addClass("layui-form-radio").append('<i class="layui-anim layui-icon">&#xe643;</i>')
-                $(v).parent().parent().parent(".datagrid-row").click(function() {
-                    radioSelect();
+                radio.addClass("layui-hide").parent().addClass("layui-form-radio").append('<i class="layui-anim layui-icon">&#xe63f;</i>')
+                radio.closest("tr").click(function() {
+                    radioSelect(this);
                 });
             });
             //选中处理图标
-            function radioSelect() {
+            function radioSelect(t) {
+                var radio = $(t).find("input[type=radio]");
+                radio[0].checked = !radio[0].checked;
                 //事件延迟等待复选框状态
                 setTimeout(function() {
-                    checkbox = view.find("input[type=radio]");
+                    checkbox = view.find("tr input[type=radio]");
                     $.each(checkbox, function(i, v) {
-                        this.checked ? $(this).parent().addClass("layui-anim-scaleSpring") : $(this).parent().removeClass("layui-anim-scaleSpring");
+                        var icon = $(this).parent().find("i");
+                        this.checked ? icon.html("&#xe643;") : icon.html("&#xe63f;");
+                        this.checked ? $(this).parent().addClass("layui-form-radioed") : $(this).parent().removeClass("layui-form-radioed");
                     });
                 }, 100)
             }
@@ -42,7 +48,7 @@ $.extend($.fn.datagrid.defaults.view, {
                 }
                 //添加样式
                 $(v).addClass("layui-hide").parent().addClass("layui-form-checkbox").attr("lay-skin", "primary").append('<i class="layui-icon layui-icon-ok"></i>')
-                $(v).parent().parent().parent(".datagrid-row").click(function() {
+                $(v).closest("tr").click(function() {
                     checkboxSelect();
                 });
             });
@@ -50,7 +56,7 @@ $.extend($.fn.datagrid.defaults.view, {
             function checkboxSelect() {
                 //事件延迟等待复选框状态
                 setTimeout(function() {
-                    checkbox = view.find("input[type=checkbox]");
+                    checkbox = view.find("tr input[type=checkbox]");
                     $.each(checkbox, function(i, v) {
                         this.checked ? $(this).parent().addClass("layui-form-checked") : $(this).parent().removeClass("layui-form-checked");
                     });
