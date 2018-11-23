7806f115d35c0173966c666d1a92bb219c70cdff
PoppinRubo <hiphopbl@aliyun.com>
2018-11-23 15:16:05 +0800

    items/application/facade/RoleFacade.php

--- items/application/facade/RoleFacade.php
+++ items/application/facade/RoleFacade.php
@@ -15,7 +15,7 @@ class RoleFacade
             if ($a["roleId"] < 0 || !is_Array(json_decode($a["ids"]))) {
                 return toJsonData(0, null, "参数错误");
             }
-            $model = new SysRoleModule();
+
             $id = trim(trim($a["ids"], "]"), "[");
             $ids = json_decode($a["ids"]);
             $data = db('sys_role_module')->whereIn('ModuleId', $id)->where(array("RoleId" => $a["roleId"]))->select();
@@ -24,49 +24,41 @@ class RoleFacade
             foreach ($data as $d) {
                 foreach ($ids as $i) {
                     if ($i == $d["ModuleId"]) {
-                        array_push($in, $i);
+                        $in["Id"][] = $d["Id"];
+                        $in["ModuleId"][] = $d["ModuleId"];
                     }
                 }
             }
-            //关联SQL生成
-            $sql = "";
+            //操作的数据
+            $data = array();
             foreach ($ids as $i) {
-                if (in_array($i, $in)) {
+                $inId = array_search($i, $in["ModuleId"]);
+                if ($inId !== false) {
                     //更新可用状态，数据复活
                     $update = array(
+                        'Id' => $in["Id"][$inId],
                         'IsValid' => $a["isRelation"] ? 1 : 0,
                         'IsDel' => $a["isRelation"] ? 0 : 1,
                         "ModifyTime" => date("Y-m-d H:i:s"),
                         "ModifyUser" => $a["operaterId"],
                     );
-                    $sql .= db('sys_role_module')->fetchSql(true)->where(array("ModuleId" => $i, "RoleId" => $a["roleId"]))->update($update);
-                    $sql .= ";";
+                    array_push($data, $update);
                 } else if ($a["isRelation"]) {
+                    //插入数据
                     $insert = array(
-                        "ModuleId" => $a["moduleId"],
+                        "ModuleId" => $i,
                         "RoleId" => $a["roleId"],
                         "CreateTime" => date("Y-m-d H:i:s"),
                         "CreateUser" => $a["operaterId"],
                         "ModifyTime" => date("Y-m-d H:i:s"),
                         "ModifyUser" => $a["operaterId"],
                     );
-                    $sql .= db('sys_role_module')->fetchSql(true)->insert($insert);
-                    $sql .= ";";
+                    array_push($data, $insert);
                 }
             }
+            $model = new SysRoleModule();
+            $model->saveAll($data);
             $baseResult->result = true;
-            // 启动事务
-            db()->startTrans();
-            try {
-                db()->execute($sql);
-                // 提交事务
-                db()->commit();
-            } catch (Exception $e) {
-                // 回滚事务
-                db()->rollback();
-                $baseResult->result = false;
-                $msg->$e->getMessage();
-            }
         } catch (Exception $e) {
             $baseResult->result = false;
             $msg->$e->getMessage();
