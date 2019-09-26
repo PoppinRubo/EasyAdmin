47ee977e2fd56224041ad220e4d5fad92c0ece67
PoppinRubo <hiphopbl@aliyun.com>
2019-09-26 18:19:21 +0800

    静态资源结构调整

---
+++ items/public/admin/global.php
@@ -0,0 +1,20 @@
+    <title>[title]</title>
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+    <link rel="stylesheet" href="/admin/static/layui/css/layui.css" media="all">
+    <link rel="stylesheet" href="/admin/static/easyui/style/easyui.css" media="all">
+    <link rel="stylesheet" href="/admin/static/style/easySkin.css" media="all">
+    <link rel="stylesheet" href="/admin/static/style/menuTree.css" media="all">
+    <link rel="stylesheet" href="/admin/static/icomoon/style.css" media="all">
+    <link rel="stylesheet" href="/admin/static/style/admin.css" media="all">
+    <!-- <link rel="stylesheet" href="/admin/static/style/theme/black.css" media="all"> -->
+    <script type="text/javascript" src="/admin/static/script/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="/admin/static/script/jquery.cookie.js"></script>
+    <script type="text/javascript" src="/admin/static/easyui/jquery.easyui.min.js"></script>
+    <script type="text/javascript" src="/admin/static/easyui/easyui-lang-zh_CN.js"></script>
+    <script type="text/javascript" src="/admin/static/script/extend/datagrid.extend.js"></script>
+    <script type="text/javascript" src="/admin/static/layui/layui.all.js"></script>
+    <script type="text/javascript" src="/admin/static/script/common.js"></script>
