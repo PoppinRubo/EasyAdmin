48cae63051940f5d83a0816b743c85d711a6d795
PoppinRubo <hiphopbl@aliyun.com>
2020-05-28 16:34:47 +0800

    datagrid.extend.js

--- items/public/admin/static/script/extend/datagrid.extend.js
+++ items/public/admin/static/script/extend/datagrid.extend.js
@@ -20,8 +20,71 @@ $.extend($.fn.treegrid.defaults.view, {
 
 //方法扩展
 $.extend($.fn.datagrid.methods, {
+    //右键菜单
     columnMenu: function (t) {
         return datagridExtend.buildMenu(t.get(0));
+    },
+    //自动合并相同数据行
+    mergeSameCells: function (jq, fields) {
+        return jq.each(function () {
+            var target = $(this);
+            if (!fields) {
+                fields = target.datagrid("getColumnFields");
+            }
+            var rows = target.datagrid("getRows");
+            var i = 0,
+                j = 0,
+                temp = {};
+            for (i; i < rows.length; i++) {
+                var row = rows[i];
+                j = 0;
+                for (j; j < fields.length; j++) {
+                    var field = fields[j];
+                    var tf = temp[field];
+                    if (!tf) {
+                        tf = temp[field] = {};
+                        tf[row[field]] = [i];
+                    } else {
+                        var tfv = tf[row[field]];
+                        if (tfv) {
+                            tfv.push(i);
+                        } else {
+                            tfv = tf[row[field]] = [i];
+                        }
+                    }
+                }
+            }
+            $.each(temp,
+                function (field, colunm) {
+                    $.each(colunm,
+                        function () {
+                            var group = this;
+                            if (group.length > 1) {
+                                var before, after, megerIndex = group[0];
+                                for (var i = 0; i < group.length; i++) {
+                                    before = group[i];
+                                    after = group[i + 1];
+                                    if (after && (after - before) == 1) {
+                                        continue;
+                                    }
+                                    var rowspan = before - megerIndex + 1;
+                                    if (rowspan > 1) {
+                                        target.datagrid('mergeCells', {
+                                            index: megerIndex,
+                                            field: field,
+                                            rowspan: rowspan
+                                        });
+                                    }
+                                    if (after && (after - before) != 1) {
+                                        megerIndex = after;
+                                    }
+                                }
+                            }
+                        }
+                    );
+                }
+            );
+        });
     }
 });
 
