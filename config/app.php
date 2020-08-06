ac5ef2a6ed0a294e986cd605e530386a9908d374
PoppinRubo <hiphopbl@aliyun.com>
2020-08-06 14:12:57 +0800

    目录调整

--- config/app.php
+++ config/app.php
@@ -24,7 +24,7 @@ return [
     'app_map' => [],
     // 域名绑定（自动多应用模式有效）
     'domain_bind' => [
-        'admi' => 'admin',
+        'admin' => 'admin',
     ],
     // 禁止URL访问的应用列表（自动多应用模式有效）
     'deny_app_list' => ['common'],
